2d276f7c9b6960e0227351dd008527bea9768a4a
Syed Zohaib Karim <karim.zohaib2004@gmail.com>
2026-04-23 18:11:06 +0530

    fix CORS for Vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://yt-summarizer-two.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
